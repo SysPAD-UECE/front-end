d1e6e162ce07cc4333b0a55642a7d72016adf92c
clara-mesquita <clara.mesquita@aluno.uece.br>
2022-10-26 20:34:19 -0300

    Change Home page

--- quasar-front/src/pages/Home.vue
+++ quasar-front/src/pages/Home.vue
@@ -1,47 +1,59 @@
 <template>
-  <q-card  class="text-h5">
-      Projeto de Proteção de Dados baseado em Técnicas de Anonimização
 
+  <q-card class="text-h5 ">
+    Projeto de Proteção de Dados baseado em Técnicas de Anonimização
 
   </q-card>
-  <q-card flat bordered class="my-card">
+  <div class="q-pa-md row">
+    <q-card class="my-card text-white col">
       <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-h6">This brings an innovative way to protect customers from other entities that may be users of
+          private entities for various government entities, through anonymization and processing in techniques. This
+          advances the state-of-the-art and existing solutions do not address data privacy mitigation and help companies
+          and government entities to comply with privacy laws.</div>
       </q-card-section>
 
-      <q-card-section class="q-pt-none">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </q-card-section>
-
-      <q-separator inset />
-
-      <q-card-section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </q-card-section>
     </q-card>
 
+    <q-card class="my-card text-white col">
+      <q-card-section>
+        <div class="q-pa-md">
+    <q-video  :ratio="16/9"
+      src="https://www.youtube.com/embed/uHKfrz65KSU"
+    />
+  </div>
+  <q-card-section class="row justify-around">
+  <div class="col">
+    <q-btn>GIT HUB</q-btn>
+  </div>
+  <div class="col">
+    <q-btn>LARCES</q-btn>
+  </div>
+</q-card-section>
+      </q-card-section>
+
+    </q-card>
+  </div>
 </template>
-  <script>
+<script>
 
-  import { defineComponent } from 'vue'
-
-
-  export default defineComponent({
-    name: 'home',
+import { defineComponent } from 'vue'
 
 
-    methods: {
-    },
-    data() {
-      return {
+export default defineComponent({
+  name: 'home',
 
-      }
-    },
-    mounted() {
+
+  methods: {
+  },
+  data() {
+    return {
+
     }
-  })
-  </script>
+  },
+  mounted() {
+  }
+})
+</script>
 
 
