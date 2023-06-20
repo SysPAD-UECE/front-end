0d4129718502a9ab303da1df87999602b5ae6fe5
clara-mesquita <clara.mesquita@aluno.uece.br>
2023-06-20 16:06:45 -0300

    Add email validation to change password feature

--- src/pages/ChangePassword.vue
+++ src/pages/ChangePassword.vue
@@ -1,12 +1,11 @@
 <template>
-
-  <img src="~assets/blue-wave.svg" class="wave" alt="login-wave">
+  <img src="~assets/blue-wave.svg" class="wave" alt="login-wave" />
   <div class="row" style="height: 90vh">
     <div class="col-0 col-md-6 flex justify-center content-center">
       <!-- <img src="~assets/login.svg" class="responsive" alt="login-image"> -->
     </div>
     <div class="col-12 col-md-6 flex content-center">
-      <q-card style='width: 80%'>
+      <q-card style="width: 80%">
         <q-card-section>
           <q-avatar size="150px" class="absolute-center" icon="face">
           </q-avatar>
@@ -14,31 +13,55 @@
         <q-card-section>
           <div class="q-pt-lg">
             <div class="col text-h6 ellipsis flex justify-center">
-              <h2 class="text-h3 text-uppercase q-my-none text-weight-regular">Change Password</h2>
+              <h2 class="text-h3 text-uppercase q-my-none text-weight-regular">
+                Change Password
+              </h2>
             </div>
           </div>
         </q-card-section>
         <q-card-section>
           <q-form class="q-gutter-md" @submit.prevent="submitNewPassword">
-            <q-input label="New Password" :type="isPwd ? 'password' : 'text'" v-model="password.newPassowrd" :rules="[
-              val => !!val || 'Password is empty'
-            ]">
+            <q-input
+              label="New Password"
+              :type="isPwd ? 'password' : 'text'"
+              v-model="password.newPassword"
+              :rules="[(val) => !!val || 'Password is empty']"
+            >
               <template v-slot:append>
-                <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer"
-                  @click="isPwd = !isPwd" />
+                <q-icon
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                />
               </template>
             </q-input>
-            <q-input label="Repeat New Password" :type="isPwd ? 'password' : 'text'" v-model="password.newPassowrdRepeat" :rules="[
-              val => !!val || 'Password is empty'
-            ]">
+
+            <q-input
+              label="Repeat New Password"
+              :type="isPwdRepeat ? 'password' : 'text'"
+              v-model="password.newPasswordRepeat"
+              :rules="[
+                (val) => !!val || 'Password is empty',
+                (val) =>
+                  val === password.newPassword || 'Passwords do not match',
+              ]"
+            >
               <template v-slot:append>
-                <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer"
-                  @click="isPwd = !isPwd" />
+                <q-icon
+                  :name="isPwdRepeat ? 'visibility_off' : 'visibility'"
+                  class="cursor-pointer"
+                  @click="isPwdRepeat = !isPwdRepeat"
+                />
               </template>
             </q-input>
             <div>
-              <q-btn class="full-width" color="primary" label="Send New Password" type="submit" rounded></q-btn>
-
+              <q-btn
+                class="full-width"
+                color="primary"
+                label="Send New Password"
+                type="submit"
+                rounded
+              ></q-btn>
             </div>
           </q-form>
         </q-card-section>
@@ -48,49 +71,72 @@
 </template>
 
 <script>
-import { Notify } from 'quasar'
-import useAuthUser from 'src/composables/UseAuthUser'
-import { defineComponent } from 'vue'
-import { mapActions, mapGetters } from 'vuex'
+import { Loading, Notify } from "quasar";
+import { defineComponent, ref } from "vue";
+import { mapActions, mapGetters } from "vuex";
+import { api } from 'src/boot/axios'
 
 export default defineComponent({
-  name: 'Login',
+  name: "ChangePassword",
   data() {
     return {
       password: {
-        newPassowrd: '',
-        newPasswordRepeat: ''
+        newPassword: "",
+        newPasswordRepeat: "",
       },
-      isPwd: true
-    }
+      isPwd: true,
+      isPwdRepeat: true
+    };
   },
   computed: {
-    ...mapGetters('auth', ['getMe']),
-    ...mapGetters('auth', ['isAuthenticated']),
-
-
+    ...mapGetters("auth", ["getMe"]),
+    ...mapGetters("auth", ["isAuthenticated"]),
   },
   methods: {
-
-    async submitNewPassword() {
-      try {
-
-        Notify.create({
-          type: 'positive',
-          message:"Password changed",
-          timeout: 1000
+    submitNewPassword() {
+      Loading.show();
+      const token = this.$route.query.token;
+      const data = {
+        new_password: this.password.newPassword,
+        repeat_new_password: this.password.newPasswordRepeat,
+      };
+      console.log("aqui")
+      console.log(data)
+      console.log(token)
+      api
+        .patch(`/password/redefine/${token}`, data)
+        .then((response) => {
+          Loading.hide();
+          this.$router.push({ name: "login" });
+          Notify.create({
+            type: "positive",
+            message: "Your password has been successfully changed",
+            timeout: 2000
+          });
         })
-      } catch (err) {
-        console.log(err)
-        Notify.create({
-          type: 'negative',
-          message: "Cannot change password. Try again later",
-          timeout: 1000
-        })
-      }
+        .catch(function (err) {
+          Loading.hide()
+          const status = err.response.status
+          this.$router.push({ name: 'login' })
+          if (status === 404) {
+          Notify.create({
+            type: "negative",
+            message: "This user is invalid. Try again later.",
+            timeout: 2000
+          });
+          this.$router.push({ name: "login" });
+          this.resendEmail()
+        } else {
+          Notify.create({
+            type: "negative",
+            message: "Oops! Something went wrong. Please try again later.",
+            timeout: 2000,
+          });
+        }
+        });
     }
-  }
-})
+  },
+});
 </script>
 
 <style scoped>
